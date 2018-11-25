bb8f988fe0f0bfad8fa80354522c8bc52a236260
Alexandre Miguel <aleronupe@gmail.com>
2018-11-25 01:34:32 -0200

    Created user with gem devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181122215958) do
+ActiveRecord::Schema.define(version: 20181125020500) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -39,6 +39,18 @@ ActiveRecord::Schema.define(version: 20181122215958) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.index ["Category_id"], name: "index_products_on_Category_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
