d1f0b1f6a2f2c6a8a900a4e2dc6830c3466d454b
Helena Goulart <helenabretasg@gmail.com>
2018-11-26 19:45:45 -0500

    Included some stuff of login scene

--- db/migrate/20181125030713_create_kart_items.rb
+++ db/migrate/20181125030713_create_kart_items.rb
@@ -1,6 +1,5 @@
 class CreateKartItems < ActiveRecord::Migration[5.0]
   def change
-    drop_table (:kart_items, if_exists: true)
     create_table :kart_items do |t|
       t.integer :quantity
       t.references :product, foreign_key: true
